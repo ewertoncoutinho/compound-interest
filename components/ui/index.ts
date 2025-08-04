3e87899a4d626d13acc7f2941c00fcd8fe4cb797
Ewerton Coutinho <ewerton.maciel@mblabs.com.br>
2025-08-04 02:05:43 -0300

    fix: imports

--- components/ui/index.ts
+++
@@ -1,2 +0,0 @@
-export { Input } from './input';
-export { Select, SelectContent, SelectValue, SelectGroup, SelectItem, SelectLabel, SelectScrollDownButton, SelectScrollUpButton, SelectSeparator, SelectTrigger} from './select';
